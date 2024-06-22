dc16d46c9563b52d716a527f96f986fdde9dc470
Gorgias26 <37674056+Gorgias26@users.noreply.github.com>
2024-06-22 20:53:55 +0200

    Add files via upload

---
+++ 1hcron.sh
@@ -0,0 +1,2 @@
+source finalCopy/.venv/bin/activate
+python3 finalCopy/multi_bybitALL.py
---
+++ secret.py
@@ -0,0 +1,11 @@
+ACCOUNTS1 = {
+    "bitget1": {
+        "public_api": "",
+        "secret_api": "",
+        "password": "",
+    },
+    "bybit": {
+        "public_api": "98E8rJXQ7Nd0rNip0n",
+        "secret_api": "AihJpXxDNREntXAyPcdM4Ufmg49bbSQdcCsU",  # Envellope Java
+    },
+}
